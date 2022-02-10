9714ff13b1d712e82e674959e29e67a09b90b99e
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-02-10 00:02:42 -0300

    configurando a conexao dos models com a database

---
+++ src/database/index.js
@@ -0,0 +1,8 @@
+import Sequelize from 'sequelize';
+import dbConfig from '../config/database';
+import Aluno from '../models/Aluno';
+
+const connection = new Sequelize(dbConfig);
+const models = [Aluno];
+
+models.forEach((model) => model.init(connection));
