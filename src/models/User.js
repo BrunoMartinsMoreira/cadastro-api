bba31111aebd7de16324b47a79518fd875b5313e
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-02-11 22:36:10 -0300

    criando model de user

---
+++ src/models/User.js
@@ -0,0 +1,49 @@
+import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [4, 255],
+            msg: 'Nome deve ter entre 4 e 255 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          isEmail: {
+            args: [4, 255],
+            msg: 'Preencha um email válido',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [10, 50],
+            msg: 'Senha deve ter entre 10 e 50 caracteres',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+    return this;
+  }
+}
