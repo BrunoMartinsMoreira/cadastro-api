95b16b4872d61e874a43e72caac12d5c311f2893
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-02-15 22:32:17 -0300

    alterando procedimento quando usuario altera o email

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/User';
 
-export default (req, res, next) => {
+export default async (req, res, next) => {
   const { authorization } = req.headers;
   /*
   enviado nos headers da requisição ex
@@ -20,8 +21,23 @@ export default (req, res, next) => {
   try {
     const data = jwt.verify(token, process.env.TOKEN_SECRET);
     const { id, email } = data;
+
+    const user = await User.findOne({
+      where: {
+        id,
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        errors: ['Usuário inválido, faça login novamente'],
+      });
+    }
+
     req.userId = id;
     req.userEmail = email;
+
     return next();
   } catch (err) {
     return res.status(401).json({
