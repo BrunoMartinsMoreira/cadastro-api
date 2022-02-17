057a456cdaa28a70024d931640cd7a06fd440552
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-02-16 22:24:39 -0300

    criando rotas de alunos e registrando no app.

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -3,7 +3,10 @@ import alunoController from '../controllers/AlunoController';
 
 const router = new Router();
 
+router.post('/', alunoController.store);
 router.get('/', alunoController.index);
+router.get('/:id', alunoController.show);
 router.put('/:id', alunoController.update);
+router.delete('/id', alunoController.delete);
 
 export default router;
