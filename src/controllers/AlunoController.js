ff3ba87a8d625cda8e8ffa7016c0edb61451c390
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-02-16 23:07:44 -0300

    criando crud de alunos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,6 +1,20 @@
 import Aluno from '../models/Aluno';
 
 class AlunoController {
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+      return res.status(200).json({
+        message: `O Aluno ${aluno.nome} foi cadastrado com sucesso`,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.message),
+      });
+    }
+  }
+
   async index(req, res) {
     try {
       const alunos = await Aluno.findAll();
@@ -10,28 +24,102 @@ class AlunoController {
     }
   }
 
-  async update(req, res) {
+  async show(req, res) {
     try {
-      const aluno = await Aluno.findByPk(req.params.id);
+      const { id } = req.params;
 
-      if (!aluno) {
-        return res.status(400).json({
-          errors: ['Aluno não encontrado.'],
+      if (!id) {
+        return req.status(400).json({
+          errors: ['Informe o ID'],
         });
       }
 
-      const novoAluno = await aluno.update(req.body);
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return req.status(400).json({
+          errors: ['Aluno não cadastrado.'],
+        });
+      }
+
       const {
-        nome, sobrenome, email, idade, peso,
-      } = novoAluno;
+        nome, sobrenome, email, idade, peso, altura,
+      } = aluno;
 
       return res.json({
-        nome, sobrenome, email, idade, peso,
+        nome, sobrenome, email, idade, peso, altura,
       });
     } catch (err) {
       console.log(err);
       return res.status(400).json({
-        Errors: err.errors.map((error) => error.message),
+        errors: err.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return req.status(400).json({
+          errors: ['Informe o ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return req.status(400).json({
+          errors: ['Aluno não cadastrado.'],
+        });
+      }
+
+      const novoAluno = await aluno.update(req.body);
+
+      const {
+        nome, sobrenome, email, idade, peso, altura,
+      } = novoAluno;
+
+      return res.json({
+        nome, sobrenome, email, idade, peso, altura,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return req.status(400).json({
+          errors: ['Informe o ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return req.status(400).json({
+          errors: ['Aluno não cadastrado.'],
+        });
+      }
+
+      await aluno.destroy();
+
+      return res.json({
+        ok: true,
+        message: 'Deletado com sucesso',
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.message),
       });
     }
   }
--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -7,6 +7,6 @@ router.post('/', alunoController.store);
 router.get('/', alunoController.index);
 router.get('/:id', alunoController.show);
 router.put('/:id', alunoController.update);
-router.delete('/id', alunoController.delete);
+router.delete('/:id', alunoController.delete);
 
 export default router;
